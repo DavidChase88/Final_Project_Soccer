c3c4dfc35ca5c2536abab2aa0c2932fd64ff95c3
DavidChase88 <68518518+DavidChase88@users.noreply.github.com>
2021-01-17 18:55:27 -0500

    Data for dashboard

---
+++ data.js
@@ -0,0 +1,164 @@
+var data = [
+  {
+    team_name: "Tottenham Hotspur FC",
+    season: "2020",
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: "2.85",
+    wins_home: "3",
+    first_team_to_score_percentage: "0.62"
+  },
+  {
+    team_name: "Manchester City FC",
+    season: "2020",
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: "2.5",
+    wins_home: "3",
+    first_team_to_score_percentage: "0.67"
+  }, 
+  {
+    team_name: "Everton FC",
+    season: "2020",
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 3.15,
+    wins_home: 3,
+    first_team_to_score_percentage: 0.54
+  },
+  {
+    team_name: "Southampton FC",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 3.31,
+    wins_home: 4,
+    first_team_to_score_percentage: 0.77
+  },
+  {
+    team_name: "Liverpool FC",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 3.69,
+    wins_home: 7,
+    first_team_to_score_percentage: 0.62
+  },
+  {
+    team_name: "Olympique Lyonnais",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 2.67,
+    wins_home: 4,
+    first_team_to_score_percentage: 0.4
+  },
+  {
+    team_name: "Paris Saint-Germain FC",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 3,
+    wins_home: 5,
+    first_team_to_score_percentage: 0.73
+  },
+  {
+    team_name: "Lille OSC M??tropole",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 2.53,
+    wins_home: 6,
+    first_team_to_score_percentage: 0.8
+  },
+  {
+    team_name: "Olympique de Marseille",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 2.46,
+    wins_home: 3,
+    first_team_to_score_percentage: 0.77
+  },
+  {
+    team_name: "Racing Club de Lens",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 3.21,
+    wins_home: 3,
+    first_team_to_score_percentage: 0.71
+  },
+  {
+    team_name: "Bayer 04 Leverkusen",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 3.08,
+    wins_home: 3,
+    first_team_to_score_percentage: 0.58
+  },
+  {
+    team_name: "Rasen Ballsport Leipzig",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 2.75,
+    wins_home: 6,
+    first_team_to_score_percentage: 0.75
+  },
+  {
+    team_name: "1. FC Union Berlin",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 3.46,
+    wins_home: 3,
+    first_team_to_score_percentage: 0.77
+  },
+  {
+    team_name: "Juventus FC",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 2.83,
+    wins_home: 4,
+    first_team_to_score_percentage: 0.67
+  },
+  {
+    team_name: "AC Milan",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 3.33,
+    wins_home: 3,
+    first_team_to_score_percentage: 0.75
+  },
+  {
+    team_name: "FC Barcelona",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 2.92,
+    wins_home: 5,
+    first_team_to_score_percentage: 0.42
+  },
+  {
+    team_name: "Real Madrid CF",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 2.69,
+    wins_home: 4,
+    first_team_to_score_percentage: 0.77
+  },
+  {
+    team_name: "Sevilla FC",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 2,
+    wins_home: 3,
+    first_team_to_score_percentage: 0.64
+  },
+  {
+    team_name: "Club Atl??tico de Madrid",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 2.42,
+    wins_home: 6,
+    first_team_to_score_percentage: 0.75
+  },
+  {
+    team_name: "Real Sociedad de F?§tbol",
+    season: 2020,
+    champions_league_prediction: "yes",
+    average_total_goals_per_match: 2.29,
+    wins_home: 3,
+    first_team_to_score_percentage: 0.64
+  }
+ ]
+ 
+ 
